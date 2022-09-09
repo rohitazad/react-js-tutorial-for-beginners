dfe1ba158d2a3d2a65bd9a9e26ba43396de677c2
rohitazad <ram9953878727@gmail.com>
2022-09-09 10:14:21 +0530

    fist chapter is close

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MyFirstComponent from './MyFirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyFirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyFirstComponent = ()=>{
+
+    return(
+        <h1>Hello i m first component</h1>
+    )
+}
+
+export default MyFirstComponent;
