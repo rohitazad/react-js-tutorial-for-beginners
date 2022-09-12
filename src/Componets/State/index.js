c1389b95ac9b6cbcb47ab9a8f76595b52c7a19ed
rohitazad <ram9953878727@gmail.com>
2022-09-12 18:50:05 +0530

    state and props

---
+++ src/Componets/State/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const State = ({mystate})=>{
+    return (
+        <>
+        <h1>{
+            `Hello  ${mystate}`}</h1>
+        </>
+    )
+}
+
+export default State;
--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Footer from './Componets/Footer';
 import Header from './Componets/Header';
+import State from './Componets/State';
 
 const MyFirstComponent = ()=>{
-
+    let [myvalue, setMyvalue] = useState('Rohit Azad');
+    const handleClick = ()=>{
+      return  (
+        setMyvalue(myvalue = 'React Js')
+      )
+    }
     return(
         <>
         <Header/>
-            <h1>Hello i m first component</h1>
+            <State mystate={myvalue}/>
+            <input value={myvalue} type="text" onChange={(e)=>{
+                setMyvalue(myvalue = e.target.value)
+            }} />
+            <button onClick={()=>{
+                handleClick();
+            }}>
+                Click me
+            </button>
         <Footer />
         </>
     )
